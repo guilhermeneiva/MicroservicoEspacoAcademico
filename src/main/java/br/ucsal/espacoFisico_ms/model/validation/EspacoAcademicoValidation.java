a3b565282d6d77d3bbfa8e4a0c73796adaf3e786
Luis Valdo <luisvso.dev@gmail.com>
2025-11-22 16:48:50 -0300

    feat(Validation): Adicionado o metodo para validacao de ids existentes

--- src/main/java/br/ucsal/espacoFisico_ms/model/validation/EspacoAcademicoValidation.java
+++ src/main/java/br/ucsal/espacoFisico_ms/model/validation/EspacoAcademicoValidation.java
@@ -1,12 +1,29 @@
 package br.ucsal.espacoFisico_ms.model.validation;
 
 import br.ucsal.espacoFisico_ms.dto.EspacoAcademicoDTO;
+import br.ucsal.espacoFisico_ms.feign.EspacoInterface;
+import br.ucsal.espacoFisico_ms.model.exception.NaoExisteException;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class EspacoAcademicoValidation {
 
+    private EspacoInterface espacoInterface;
+
+    public EspacoAcademicoValidation(EspacoInterface espacoInterface) {
+        this.espacoInterface = espacoInterface;
+    }
+
     public boolean existId(EspacoAcademicoDTO dto) {
         return dto != null;
+    }
+
+    public void existsSoftware(EspacoAcademicoDTO dto) {
+        for (Long id : dto.softwareIds()) {
+            if (!espacoInterface.exists(id)) {
+                throw new NaoExisteException("O software de id " + id + " não existe");
+            }
+        }
     }
 }
